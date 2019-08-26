b0a472e9bc38d91c839b919b81437571deb3be6a
Noah Flint <noah.flint24@gmail.com>
2019-08-26 16:29:18 -0600

    added many to many relationship to meal model
    Co-authored-by: Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>

--- models/meal.js
+++ models/meal.js
@@ -1,10 +1,10 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Meal = sequelize.define('Meal', {
-    name: DataTypes.STRING
+    name: {type: DataTypes.STRING, allowNull: false}
   }, {});
   Meal.associate = function(models) {
-    Meal.belongsToMany(models.Food, {through models.MealFood, as: 'foods'})
+    Meal.belongsToMany(models.Food, {through: models.MealFood})
   };
   return Meal;
 };
