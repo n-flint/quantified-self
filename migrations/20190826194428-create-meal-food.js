1f3082ba4e8eeb1fe941dc1c3871b231974c3c76
Noah Flint <noah.flint24@gmail.com>
2019-08-26 13:56:39 -0600

    created a migration for meal and mealFood

---
+++ migrations/20190826194428-create-meal-food.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('MealFoods', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      FoodId: {
+        type: Sequelize.INTEGER,
+        refereneces: {
+          model: 'Food',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        allowNull: false
+      },
+      MealId: {
+        type: Sequelize.INTEGER,
+        refereneces: {
+          model: 'Meals',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('MealFoods');
+  }
+};
