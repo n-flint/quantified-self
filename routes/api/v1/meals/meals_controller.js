befefea5c3420404ef99d8f628e178dbf2c88e81
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-08-27 15:31:45 -0600

    Add post router for adding foods to meals

--- routes/api/v1/meals/meals_controller.js
+++ routes/api/v1/meals/meals_controller.js
@@ -1,31 +1,45 @@
 var express = require("express");
 var router = express.Router();
-var foods = require('../../../../models').Food;
 var Meal = require('../../../../models').Meal;
+var Food = require('../../../../models').Food;
+var MealFood = require('../../../../models').MealFood;
 var defaultHeader = ["Content-Type", "application/json"];
 
 /* GET all meals */
 router.get("/", async function (req, res, next) {
   try {
     let meals = await Meal.findAll({
-      include: foods
+      include: [{ model: Food }]
     });
     res.setHeader(...defaultHeader);
     res.status(200).send(JSON.stringify(meals, ['id', 'name', 'Food', 'id', 'name', 'calories']));
-  } catch {
+  } catch (error) {
     res.setHeader(...defaultHeader);
     res.status(500).send({ error })
   }
 });
 
-module.exports = router;
+/* Add food to meal */
+router.post("/:meal_id/foods/:food_id", async function (req, res, next) {
+  try {
+    let meal = await Meal.findOne({
+      where: {id: req.params.meal_id}
+    })
+    let food = await Food.findOne({
+      where: {id: req.params.food_id}
+    })
+    let mealfood = await MealFood.create({
+      FoodId: req.params.food_id,
+      MealId: req.params.meal_id
+    })
+    let message = await `Successfully added ${food.name} to ${meal.name}`
+    res.setHeader(...defaultHeader);
+    res.status(201).send(JSON.stringify(message));
+  } catch {
+    let error = await 'Food Not Added to Meal'
+    res.setHeader(...defaultHeader);
+    res.status(404).send({ error })
+  }
+});
 
-// User.findAll({
-//   include: [{
-//     model: Project,
-//     through: {
-//       attributes: ['createdAt', 'startedAt', 'finishedAt'],
-//       where: { completed: true }
-//     }
-//   }]
-// });
+module.exports = router;
