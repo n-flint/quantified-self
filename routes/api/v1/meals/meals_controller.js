e08bd70b44ab4875a7f305f9060179c6b845027f
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2019-08-27 17:33:01 -0600

    Add delete function to meals route with sad path
    Co-Authored-By: null <noah.flint@gmail.com>

--- routes/api/v1/meals/meals_controller.js
+++ routes/api/v1/meals/meals_controller.js
@@ -24,12 +24,11 @@ router.get("/:meal_id/foods", async function (req, res, next) {
   try {
     let meal = await Meal.findOne({
       where: {id: req.params.meal_id},
-      include: foods
+      include: [{ model: Food }]
     });
     res.setHeader(...defaultHeader);
     res.status(200).send(JSON.stringify(meal, ['id', 'name', 'Food', 'id', 'name', 'calories']));
-  } catch {
-    let error = 'Meal Not Found'
+  } catch (error) {
     res.setHeader(...defaultHeader);
     res.status(404).send({ error })
   }
@@ -53,5 +52,22 @@ router.post("/:meal_id/foods/:food_id", async function (req, res, next) {
     res.status(201).send(JSON.stringify(message));
   } catch {
     let error = await 'Food Not Added to Meal'
+    res.setHeader(...defaultHeader);
+    res.status(404).send({ error })
+  }
+});
 
+router.delete("/:meal_id/foods/:id", async function (req, res, next) {
+  try {
+    let mealfood = await MealFood.findOne({
+      where: {MealId: req.params.meal_id, FoodId: req.params.id}
+    })
+    await mealfood.destroy();
+    res.setHeader(...defaultHeader);
+    res.status(204).send();
+  } catch (error) {
+    res.setHeader(...defaultHeader);
+    res.status(404).send({ error })
+  }
+});
 module.exports = router;
